9847cce274e2bf031fac10985cf4fa96fb2ed5c7
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-09-15 14:56:18 +0300

    job-description

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,36 +1,27 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import styles                                  from './nearbyjobcard.style.js'
-import {checkImageURL}                         from "../../../../utils/index"
+import { checkImageURL }                       from "../../../../utils/index"
 
-const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
+const NearbyJobCard = ({job, handleNavigate}) => {
   return (
-    <TouchableOpacity
-      style={styles.container(selectedJob, item)}
-      onPress={() => handleCardPress(item)}
-    >
-      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
         <Image
-          source={{ uri: checkImageURL(item.employer_logo)
-              ? item.employer_logo
-              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NB1CW7diNmGXJfMicpY9eXHKV4sqz05H.jpg'
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NB1CW7diNmGXJfMicpY9eXHKV4sqz05H.jpg',
           }}
           resizeMode={`contain`}
           style={styles.logoImage}
         />
       </TouchableOpacity>
-      <Text style={styles.companyName} numberOfLines={1}>
-        {item.employer_name}
-      </Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
-          {item.job_title}
-        </Text>
-        <Text style={styles.location}>
-          {item.job_country}
-        </Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>{job.job_title}</Text>
+        <Text style={styles.jobType}>{job.job_employement_type}</Text>
       </View>
     </TouchableOpacity>
   )
 }
 
-export default PopularJobCard
+export default NearbyJobCard
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { icons }             from "../../../constants"
+import React                 from 'react'
+import { View, Text, Image } from 'react-native'
+import styles                from './company.style'
+import { checkImageURL }     from '../../../utils'
 
-import styles from './company.style'
-
-const Company = () => {
+const Company = ({companyLogo, jobTitle, companyName, location}) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NB1CW7diNmGXJfMicpY9eXHKV4sqz05H.jpg'
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName}</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            style={styles.locationImage}
+            resizeMode="contain"
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   )
 }
